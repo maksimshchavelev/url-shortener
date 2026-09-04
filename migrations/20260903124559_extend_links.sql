19acc2a35dbb01527fcfe69c9fc00f157cb13eb0
Maksim <maksimshchavelev@gmail.com>
2026-09-04 18:49:26 +0300

    rename created_ip to creator_ip

--- migrations/20260903124559_extend_links.sql
+++ migrations/20260903124559_extend_links.sql
@@ -3,4 +3,4 @@ ALTER TABLE links
     ADD COLUMN clicks_limit BIGINT      DEFAULT NULL,
     ADD COLUMN created_at   TIMESTAMPTZ DEFAULT NOW()     NOT NULL,
     ADD COLUMN expires_at   TIMESTAMPTZ DEFAULT NULL,
-    ADD COLUMN created_ip   VARCHAR(45) DEFAULT '0.0.0.0' NOT NULL -- maximum IPv6 length
+    ADD COLUMN creator_ip   VARCHAR(45) DEFAULT '0.0.0.0' NOT NULL -- maximum IPv6 length
