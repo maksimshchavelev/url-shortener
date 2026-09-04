3835437babf698a10cef50db7b9b03f1020ab728
Maksim <maksimshchavelev@gmail.com>
2026-09-04 12:22:20 +0300

    create_short_code accepts created ip, lifetime and clicks limit now

--- src/domain/link_service.rs
+++ src/domain/link_service.rs
@@ -1,15 +1,25 @@
 use crate::domain::{Error, FetchResult, OriginalUrl, ShortCode};
 use async_trait::async_trait;
+use chrono::Duration;
 
 /// Describes service that working with links. It's useful if
 /// you need to create short code from original URL, or fetch
 /// original URL by short code
 #[async_trait]
 pub trait LinkService: Send + Sync {
-    /// Create short code from original URL
+    /// Create short code from original URL while also retaining
+    /// the IP address of the person who created the link (`created_ip`)
+    /// and sets lifetime of a short code(`lifetime`) with limit of
+    /// clicks (`clicks_limit`)
     /// # Returns
     /// Short code saved to `Repository` or `domain::Error`
-    async fn create_short_code(&self, url: OriginalUrl) -> Result<ShortCode, Error>;
+    async fn create_short_code(
+        &self,
+        url: OriginalUrl,
+        created_ip: String,
+        lifetime: Option<Duration>,
+        clicks_limit: Option<i64>,
+    ) -> Result<ShortCode, Error>;
 
     /// Fetch original URL by short code
     /// # Returns
