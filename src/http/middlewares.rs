75be6fa5b6438c6fe8205b504b94aaafcd901c72
Maksim <maksimshchavelev@gmail.com>
2026-09-04 12:39:57 +0300

    add the ip_extractor middleware and move the IP extraction logic into it

--- src/http/middlewares.rs
+++ src/http/middlewares.rs
@@ -1,4 +1,8 @@
+use crate::http::client_ip::ClientIP;
+use axum::body::Body;
+use axum::http::StatusCode;
 use axum::{extract::ConnectInfo, extract::Request, middleware::Next, response::Response};
+use log::error;
 use std::net::SocketAddr;
 use tracing::{Instrument, info_span};
 
@@ -7,11 +11,31 @@ pub struct Middlewares;
 
 impl Middlewares {
     /// Creates logging span with user IP
+    /// Uses `ip_extractor` middleware
+    pub async fn ip_logger(request: Request, next: Next) -> Response {
+        let ip = match request.extensions().get::<ClientIP>().cloned() {
+            Some(ip) => ip,
+            None => {
+                error!(
+                    "Failed to receive IP in ip_logger middleware, did you forget to apply ip_extractor middleware?"
+                );
+                return Response::builder()
+                    .status(StatusCode::INTERNAL_SERVER_ERROR)
+                    .body(Body::empty())
+                    .unwrap();
+            }
+        };
+
+        let span = info_span!("Request", client_ip = ip.0);
+        next.run(request).instrument(span).await
+    }
+
+    /// Extracts client IP address
     /// Prefers `X-Real-IP` for determine user IP. If `X-Real-IP` header not provided,
     /// uses socket address to determine user IP
-    pub async fn ip_logger(
+    pub async fn ip_extractor(
         ConnectInfo(connect): ConnectInfo<SocketAddr>,
-        request: Request,
+        mut request: Request,
         next: Next,
     ) -> Response {
         let ip = request
@@ -21,7 +45,7 @@ impl Middlewares {
             .and_then(|value| Some(value.to_string()))
             .unwrap_or_else(|| connect.ip().to_string());
 
-        let span = info_span!("Request", client_ip = ip);
-        next.run(request).instrument(span).await
+        request.extensions_mut().insert(ClientIP(ip.clone()));
+        next.run(request).await
     }
 }
