552a3bc76ac399411a2cf154012e004abfc74f4b
Maksim <maksimshchavelev@gmail.com>
2026-09-07 11:57:26 +0300

    add prometheus_handle to AppState

--- src/domain/app_state.rs
+++ src/domain/app_state.rs
@@ -1,14 +1,21 @@
 use crate::domain::LinkService;
+use axum_prometheus::metrics_exporter_prometheus::PrometheusHandle;
 
 /// Represents common application state
 pub struct AppState {
     /// `LinkService` implementation
     pub link_service: Box<dyn LinkService>,
+
+    /// Prometheus handle
+    pub prometheus_handle: PrometheusHandle,
 }
 
 impl AppState {
-    /// Create a new `AppState` with `LinkService` and `server_url`
-    pub fn new(link_service: Box<dyn LinkService>) -> Self {
-        Self { link_service }
+    /// Create a new `AppState` with `LinkService` and `prometheus_handle`
+    pub fn new(link_service: Box<dyn LinkService>, prometheus_handle: PrometheusHandle) -> Self {
+        Self {
+            link_service,
+            prometheus_handle,
+        }
     }
 }
